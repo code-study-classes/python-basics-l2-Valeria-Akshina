dae534411ad7e3a61c389103e96fcf6c7ba83e9a
Valeria Akshina <aksinalera@gmail.com>
2026-04-01 08:40:46 +0000

    3 step

--- index.py
+++ index.py
@@ -3,3 +3,6 @@ def update_profile(user_id, **kwargs):
 
 def get_domains(emails):
     return map(lambda email: email.split('@')[1], emails)
+
+def filter_target_audience(users):
+    return filter(lambda user: user['age'] >= 18 and user['is_premium'], users)

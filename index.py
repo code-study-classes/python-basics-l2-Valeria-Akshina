499cf70cddd7118377c458a155979af638b4e0b4
Valeria Akshina <aksinalera@gmail.com>
2026-04-01 08:46:57 +0000

    done

--- index.py
+++ index.py
@@ -6,3 +6,12 @@ def get_domains(emails):
 
 def filter_target_audience(users):
     return filter(lambda user: user['age'] >= 18 and user['is_premium'], users)
+
+def build_response(status_code, *errors, **payload):
+    return {'status': status_code, 'errors': errors, 'data': payload}
+
+def calculate_total_spent(transactions):
+    total = 0
+    for transaction in transactions:
+        total = total + transaction['amount']
+    return total
